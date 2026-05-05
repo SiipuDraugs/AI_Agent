a53f37e3aca4e979638fa9dc2665ddb2a1582922
SiipuDraugs <martinsfreimanis5@gmail.com>
2026-05-05 13:34:42 +0300

    servers unavailable

--- main.py
+++ main.py
@@ -12,4 +12,8 @@ response = client.models.generate_content(
     model = "gemini-2.5-flash", 
     contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
-print(response.text)
+if response.usage_metadata == None:
+    raise RuntimeError('No metadata found')
+print(f'User prompt: {contents}')
+print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}')
+print(f'Response: \n{response.text}')
